b4db5a48d267049e82afc36e3232d5c50efee595
Dave Bush <github@david-bush.co.uk>
2016-03-08 22:35:52 +0000

    PEP8 Fixes.

--- guardfile-generator.py
+++ guardfile-generator.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 import os
-from itertools import chain
 from functools import partial
 from glob import glob
 
+
 def python():
     print 'Python'
+
+
 def ruby():
     print 'Ruby'
+
 
 def _count_by_extension(path, extension):
     return len([y for x in os.walk(path) for y in glob(
@@ -16,5 +19,3 @@ def _count_by_extension(path, extension):
 count = partial(_count_by_extension, '.')
 
 ruby() if count('*.rb') > count('*.py') else python()
-
-# print list(count_by_extension('.', '*.py'))
