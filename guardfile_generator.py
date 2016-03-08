f74a6e4509cdba2005f8dc047a37da5227a5e353
Dave Bush <github@david-bush.co.uk>
2016-03-08 22:38:29 +0000

    Renamed guardfile generator.

---
+++ guardfile_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+from functools import partial
+from glob import glob
+
+
+def python():
+    print 'Python'
+
+
+def ruby():
+    print 'Ruby'
+
+
+def _count_by_extension(path, extension):
+    return len([y for x in os.walk(path) for y in glob(
+        os.path.join(x[0], extension))])
+count = partial(_count_by_extension, '.')
+
+ruby() if count('*.rb') > count('*.py') else python()
