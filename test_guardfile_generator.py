6b07ebef45b265dc75f1572d19a8529a9f56c222
Dave Bush <github@david-bush.co.uk>
2016-03-09 18:31:57 +0000

    Fixed unit tests.

--- test_guardfile_generator.py
+++ test_guardfile_generator.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import unittest
 
-from guardfile_generator import _count_by_extension
+from guardfile_generator import count_by_extension
 
 
 class HelperFunctions(object):
@@ -11,11 +11,18 @@ class HelperFunctions(object):
         self.create_files(primary, secondary)
 
     def create_files(self, primary, secondary):
+        primary_count, secondary_count = 5, 4
         os.mkdir(self.directory)
-        for i in range(5):
+        for i in range(primary_count):
             open('{0}/test_{1}.{2}'.format(self.directory, i, primary), 'w+')
-        for i in range(4):
+        for i in range(secondary_count):
             open('{0}/test_{1}.{2}'.format(self.directory, i, secondary), 'w+')
+
+        # Sanity check:
+        primary_files = [x for x in os.listdir(self.directory) if x.endswith(primary)]
+        secondary_files = [x for x in os.listdir(self.directory) if x.endswith(secondary)]
+        self.assertEqual(primary_count, len(primary_files))
+        self.assertEqual(secondary_count, len(secondary_files))
 
 
 class EnvironmentDetectorTest(HelperFunctions, unittest.TestCase):
@@ -28,11 +35,11 @@ class EnvironmentDetectorTest(HelperFunctions, unittest.TestCase):
 
     def test_python_detection(self):
         self.generate_data('py')
-        print _count_by_extension('test_data', 'py')
+        self.assertEqual(count_by_extension('test_data', 'py'), 5)
 
     def test_ruby_detection(self):
         self.generate_data('rb')
-        print _count_by_extension('test_data', 'rb')
+        self.assertEqual(count_by_extension('test_data', 'rb'), 5)
 
 if __name__ == '__main__':
     unittest.main()
