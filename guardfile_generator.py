0f23ef887aff354fe3772b705b4743d3e5ea49d3
Dave Bush <github@david-bush.co.uk>
2016-03-09 19:13:14 +0000

    Moved out Guardfile variable for mocking.

--- guardfile_generator.py
+++ guardfile_generator.py
@@ -2,15 +2,35 @@
 
 import os
 
-
-def python():
-    print 'Files predominantly Python'
+GUARDFILE = 'Guardfile'
 
 
-def ruby():
-    print 'Files predominantly Ruby'
+class Python(object):
+    """
+    Infer from pip what packages are present.
+    """
+    def run_pip_freeze(self):
+        try:
+            import pip
+        except ImportError:
+            pass
+        else:
+            print pip.main('freeze')
+
+
+class Ruby(object):
+    pass
 
 
 def count_by_extension(path, extension):
     return len([y for __, __, x in os.walk(
         path) for y in x if y.endswith(extension)])
+
+
+def write_guardfile(ext, command):
+    with open(GUARDFILE, 'w') as guard:
+        guard.write('guard :shell do\n')
+        guard.write('watch /.*.rb/ do\n')
+        guard.write('`rake test:integration`\n')
+        guard.write('end\n')
+        guard.write('end\n')
