f899d11e267bf7b92832a398735042a2b9548d6c
Dave Bush <github@david-bush.co.uk>
2016-03-09 08:40:49 +0000

    Simplified generator.

--- guardfile_generator.py
+++ guardfile_generator.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 import os
-from functools import partial
-from glob import glob
 
 
 def python():
@@ -13,9 +11,9 @@ def ruby():
     print 'Files predominantly Ruby'
 
 
-def _count_by_extension(path, extension):
-    return len([y for x in os.walk(path) for y in glob(
-        os.path.join(x[0], extension))])
-count = partial(_count_by_extension, '.')
+def count_by_extension(path, extension):
+    return len([y for __, __, x in os.walk(
+        path) for y in x if y.endswith(extension)])
 
-ruby() if count('*.rb') > count('*.py') else python()
+
+
