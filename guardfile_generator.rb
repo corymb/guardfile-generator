91dc11b66281fd0be8283059dcb81cfa6dfc0283
Dave Bush <github@david-bush.co.uk>
2016-03-12 10:12:45 +0000

    Added Ruby test strings.

--- guardfile_generator.rb
+++ guardfile_generator.rb
@@ -1,5 +1,5 @@
 require 'find'
-
+require 'rubygems'
 
 def get_file_count(extension)
   files = []
@@ -10,7 +10,40 @@ def get_file_count(extension)
 end
 
 def write_guardfile(extension, test_string)
+content = %{
+guard :shell do
+  watch /.*.#{extension}/ do
+    `#{test_string}`
+  end
+end
+}
+  File.write('Guardfile_dev', content)
 end
 
-project_type = get_file_count('rb') < get_file_count('py') ? 'py' : 'rb'
-puts project_type
+def get_test_string(extension)
+  return extension == 'rb' ? get_ruby_test_string : get_python_test_string
+end
+
+def get_ruby_test_string
+  if defined? 'minitest'
+    return 'ruby test/*.rb'
+  end
+  if defined? 'rspec'
+    return 'rspec spec'
+  end
+  if defined? 'rails'
+    return 'bundle exec rspec spec'
+  end
+  installed_gems =  Gem::Specification.sort_by{
+    |gem| [gem.name.downcase] }.group_by{ |gem| gem.name }
+  raise "Couldn't find test runner in #{installed_gems.keys}"
+end
+
+def get_python_test_string
+  puts 'Python'
+end
+
+extension = get_file_count('rb') < get_file_count('py') ? 'py' : 'rb'
+test_string = get_test_string(extension)
+puts test_string
+write_guardfile(extension, test_string)
